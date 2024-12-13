40a196e8574fea1979100283540441e6ec8f4627
Wouter Hielkema <96848463+WouterHielkema@users.noreply.github.com>
2024-12-13 12:09:13 +0100

    Delete logs/builder.py

--- logs/builder.py
+++
@@ -1,8 +0,0 @@
-# 2024-11-29T11:35:07.595042600
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="C:/Users/lucmb/Desktop/school/jaar_4/KBS/ClimbingGameSoftware")
-
-vitis.dispose()
-
